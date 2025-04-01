01b96630525e51863023209182715d46cf5e88fe
infopcgood <112328126+infopcgood@users.noreply.github.com>
2025-04-01 19:55:10 +0900

    Make basic project structure, add some basic packages

---
+++ src/screen_dispatcher_pkg/screen_dispatcher_pkg/screen_dispatcher.py
@@ -0,0 +1,86 @@
+import numpy as np
+import PIL
+from PIL import ImageDraw
+import adafruit_blinka_raspberry_pi5_piomatter as piomatter
+
+import rclpy
+from rclpy.node import Node
+from rclpy.parameter import Parameter
+from interface_pkg.msg import Image
+from interface_pkg.srv import ControlRequest
+
+SCREEN_WIDTH = 64
+SCREEN_HEIGHT = 32
+SCREEN_CHAINED = 1
+ROTATE_SECOND_SCREEN = 1
+FLIP_SECOND_SCREEN = 0
+
+SCREEN_BRIGHTNESS = 80
+
+DOMINATING_NODE_NAME_DEFAULT = 'none'
+
+## ScreenDispatcherNode class
+# The screen dispatcher controls what node is in control of the screen.
+# It then receives the frame data in interface_pkg.msg.Image format and 
+# displays it onto the screen
+class ScreenDispatcherNode(Node):
+    ## __init__ function
+    # This function does not take any parameters.
+    # This function also initializes the LED Matrix display and creates a subscription.
+    def __init__(self):
+        super().__init__('screen_dispatcher_node')
+        # Set dominating node name parameter to default value
+        self.declare_parameter('dominating_node_name', DOMINATING_NODE_NAME_DEFAULT)
+        # LED Matrix initialization
+        self.geometry = piomatter.Geometry(width=SCREEN_WIDTH, height=SCREEN_HEIGHT * (SCREEN_CHAINED + 1), n_addr_lines=4, rotation=piomatter.Orientation.Normal)
+        self.canvas = PIL.Image.new('RGB', (SCREEN_WIDTH, SCREEN_HEIGHT * (SCREEN_CHAINED + 1)), (0, 0, 0))
+        self.draw = ImageDraw.Draw(self.canvas)
+        self.framebuffer = np.asarray(self.canvas) + 0
+        self.matrix = piomatter.PioMatter(colorspace=piomatter.Colorspace.RGB888Packed,
+                             pinout=piomatter.Pinout.AdafruitMatrixBonnet,
+                             framebuffer=self.framebuffer,
+                             geometry=self.geometry)
+        # Subscribe to /screen_display topic
+        self.display_sub = self.create_subscription(Image, 'screen_display', self.display_sub_cb, 10)
+        # Declare /request_screen_control service
+        self.request_srv = self.create_service(ControlRequest, 'request_screen_control', self.request_srv_cb)
+
+    ## display_sub_cb function
+    # When /screen_display topic is received, check if sender is controlling the screen
+    # and display the image.
+    def display_sub_cb(self, image):
+        if SCREEN_WIDTH != image.width:
+            return
+        if SCREEN_HEIGHT != image.height:
+            return
+        if image.sender != self.get_parameter('dominating_node_name').get_parameter_value().string_value:
+            return
+        frame = (np.array(image.data).reshape((SCREEN_HEIGHT, SCREEN_WIDTH, 3)) * (SCREEN_BRIGHTNESS / 100)).astype(np.uint8)
+        self.canvas.paste(PIL.Image.fromarray(frame), (0, 0))
+        self.canvas.paste(PIL.Image.fromarray(frame[::(-1 if ROTATE_SECOND_SCREEN else 0), ::(-1 if ROTATE_SECOND_SCREEN and not FLIP_SECOND_SCREEN else 1), :]), (0, SCREEN_HEIGHT))
+        self.framebuffer[:] = np.asarray(self.canvas)
+        self.matrix.show()
+
+    ## request_sub_cb function
+    # When /request_screen_control topic is received, give screen control.
+    def request_srv_cb(self, request, response):
+        self.get_logger().info(f'Node {request.sender} has asked for screen control!')
+        self.set_parameters([Parameter('dominating_node_name', Parameter.Type.STRING, request.sender)])
+        response.success = True
+        response.message = ""
+        self.get_logger().info(f'Successfully gave screen control to {request.sender}!')
+        return response
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ScreenDispatcherNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info('KeyboardInterrupt, shutting down dispatcher...')
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ src/video_player_pkg/video_player_pkg/video_player.py
@@ -0,0 +1,83 @@
+import numpy as np
+import cv2
+from PIL import Image, ImageDraw
+import time
+from pydub import AudioSegment
+from pydub.playback import play
+import threading
+
+import rclpy
+from rclpy.node import Node
+from interface_pkg.msg import Image
+from interface_pkg.srv import ControlRequest
+
+SCREEN_WIDTH = 64
+SCREEN_HEIGHT = 32
+
+## VideoPlayerNode class.
+# Inherits the Node class from rclpy, and plays a video from the given filename.
+# Plays the audio at the same time with the video.
+# The video will stop at either the end of the video or an error, but the audio
+# will only stop at the end of the video (it will keep playing when an error happens)
+class VideoPlayerNode(Node):
+    ## __init__ function.
+    # Gets filename as string parameter, and automatically detects file format.
+    # It also detects the FPS of the given video, and will set the timer according to it.
+    # Audio is played at the same time by using the 'threading' library.
+    def __init__(self, filename):
+        super().__init__('video_player_node')
+        # Get video capture and data
+        self.cap = cv2.VideoCapture(filename)
+        fps = self.cap.get(cv2.CAP_PROP_FPS)
+        # Make publisher and load audio
+        self.pub = self.create_publisher(Image, 'screen_display', 10)
+        self.audio = AudioSegment.from_file(filename, filename.strip().split('.')[-1])
+        # Ask for screen control and play video/audio
+        request_cli = self.create_client(ControlRequest, 'request_screen_control')
+        request = ControlRequest.Request()
+        request.sender = 'video_player_node'
+        self.get_logger().info('Asking screen control permission...')
+        request_cli.wait_for_service()
+        future = request_cli.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+        if future.result().success:
+            self.get_logger().info('Got screen control permission!')
+            self.create_timer(1 / fps, self.frame_timer_cb)
+            threading.Thread(target=play, args=(self.audio,)).start()
+        else:
+            self.get_logger().info('Failed to get screen control! Exiting...')
+            self.destroy_node()
+
+    ## frame_timer_cb function
+    # Executed each frame (1/FPS).
+    # Tries to read a frame from the loaded video and if successful publishes the frame
+    # as an object of interface_pkg.msg.Image
+    def frame_timer_cb(self):
+        if not self.cap.isOpened(): # If video has ended
+            self.destroy_node()
+            return
+        ret, frame = self.cap.read() # Try to read frame from video
+        if not ret: # If frame could not be read
+            self.destroy_node()
+            return
+        # Make an Image msg object to send
+        image = Image()
+        image.sender = 'video_player_node'
+        image.height = SCREEN_HEIGHT
+        image.width = SCREEN_WIDTH
+        image.data = (cv2.resize(frame, (SCREEN_WIDTH, SCREEN_HEIGHT))).astype(np.uint8).reshape((-1,)) # Data needs to be reshaped into a 1D array
+        self.pub.publish(image) # Send object
+
+# Simple main function
+def main(args=None):
+    rclpy.init(args=args)
+    node = VideoPlayerNode(input('Input video filename: '))
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info('KeyboardInterrupt has occured, shutting down.')
+    finally:
+        if node:
+            node.destroy_node()
+        rclpy.shutdown()
+
