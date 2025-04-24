e3092871f617f3a8c205e68234a2e49605064630
infopcgood <112328126+infopcgood@users.noreply.github.com>
2025-04-25 00:08:59 +0900

    Fix typo

--- src/apds9960_input_pkg/apds9960_input_pkg/apds9960_input.py
+++ src/apds9960_input_pkg/apds9960_input_pkg/apds9960_input.py
@@ -13,7 +13,7 @@ class APDS9960InputNode(Node):
     def __init__(self):
         super().__init__('APDS9960_input_node')
         self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_apds9960.apds9960.APDS9960(i2c)
+        self.sensor = adafruit_apds9960.apds9960.APDS9960(self.i2c)
         self.proximity_live_pub = self.create_publisher(ProximityLiveData, 'proximity_live_data', 10)
         self.proximity_trigger_pub = self.create_publisher(ProximityTrigger, 'proximity_trigger', 10)
         self.previous_proximity = 255
